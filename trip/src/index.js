fb28a069b10d80e55fcfe9b8769c83fad0e7131c
sudhanvishal-2001 <sudhanvishal2001@gmail.com>
2024-11-26 21:20:37 +0530

    changed in landing page

--- trip/src/index.js
+++ trip/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Details } from './Components/Details';
+import { LandingPage } from './Components/LandingPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Details />
+    <App />
   </React.StrictMode>
 );
 
