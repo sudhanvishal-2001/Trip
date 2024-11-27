a489cc4b45f6ed468c74cf33efe96ade82b82e42
sudhanvishal-2001 <sudhanvishal2001@gmail.com>
2024-11-27 19:17:22 +0530

    commit27/11/24

--- trip/src/Components/FirstPage.js
+++ trip/src/Components/FirstPage.js
@@ -1,37 +1,27 @@
 import React, { useState, useEffect } from "react";
-import Card from "react-bootstrap/Card";
 import "../Styles/FirstPage.css";
 import bg1 from "../img1/page2bg.png";
 import bg2 from "../img1/page2bg2.png";
 import logo from "../img/logo.png";
 import profilelogo from "../img/profilelogo.png";
 import belllogo from "../img/belllogo.png";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { useSpring, animated } from "react-spring";
+import svgplan from "../img1/svgplan.png";
 import scimg1 from "../img1/scimg1.png";
 import scimg2 from "../img1/scimg2.png";
 import scimg3 from "../img1/scimg3.png";
 import mcv1 from "../img1/mcv1.png";
 import mcv2 from "../img1/mcv2.png";
 import mcv3 from "../img1/mcv3.png";
-import linkicon from "../img1/linkicon.png";
-import svgplan from "../img1/svgplan.png";
-import { FaLongArrowAltLeft } from "react-icons/fa";
-import { FaLongArrowAltRight } from "react-icons/fa";
-import { GoLocation } from "react-icons/go";
-import { IoFootstepsSharp } from "react-icons/io5";
-import { IoTelescope } from "react-icons/io5";
-import { FaLink } from "react-icons/fa6";
-import { FaRegBookmark } from "react-icons/fa";
-import { ImArrowUpRight } from "react-icons/im";
-import { MdKeyboardDoubleArrowRight ,MdClose} from "react-icons/md";
-import { useSpring, animated } from "react-spring";
 
-
-import Carousel  from './Carousel';
+import ImageSlider from './ImageSlider';  // Import the new ImageSlider component
 
 export const FirstPage = () => {
   const [activeView, setActiveView] = useState("single");
   const [currentIndexes, setCurrentIndexes] = useState([0, 1, 2]);
-  
+  const [direction, setDirection] = useState("");
 
   const cardDetails = [
     {
@@ -98,43 +88,49 @@ export const FirstPage = () => {
     },
   ];
 
-
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggleSidebar = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    const closeSidebar = () => {
-      setIsOpen(false);
-    };
-  
-
   const moveNext = () => {
+    setDirection("right"); // Set direction to right
     setCurrentIndexes((prevIndexes) => {
-      const data = activeView === "single" ? cardDetails[0].singlecityview.details : cardDetails[1].multicityview.details;
-      return prevIndexes.map((index) => (index + 1) % data.length);
+      const data =
+        activeView === "single"
+          ? cardDetails[0].singlecityview.details
+          : cardDetails[1].multicityview.details;
+      const updatedIndexes = prevIndexes.map((index) => (index + 1) % data.length);
+      return updatedIndexes;
     });
+
+    setTimeout(() => {
+      setDirection(""); // Reset direction to allow for smooth animation transition
+    }, 500);
   };
 
-  
   const movePrevious = () => {
+    setDirection("left"); // Set direction to left
     setCurrentIndexes((prevIndexes) => {
-      const data = activeView === "single" ? cardDetails[0].singlecityview.details : cardDetails[1].multicityview.details;
-      return prevIndexes.map((index) => (index - 1 + data.length) % data.length);
+      const data =
+        activeView === "single"
+          ? cardDetails[0].singlecityview.details
+          : cardDetails[1].multicityview.details;
+      const updatedIndexes = prevIndexes.map(
+        (index) => (index - 1 + data.length) % data.length
+      );
+      return updatedIndexes;
     });
+
+    setTimeout(() => {
+      setDirection(""); // Reset direction to allow for smooth animation transition
+    }, 500);
   };
 
-  
   useEffect(() => {
-    const interval = setInterval(moveNext, 3000);
-    return () => clearInterval(interval);
-  }, [activeView]);
+    const interval = setInterval(moveNext, 4000);
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, [activeView, currentIndexes]);
 
-    // Get the correct data based on active view
-    const viewData =activeView === "single" ? cardDetails[0].singlecityview.details : cardDetails[1].multicityview.details;
+  const viewData =
+    activeView === "single" ? cardDetails[0].singlecityview.details : cardDetails[1].multicityview.details;
 
-    const backgroundImage = activeView === "single" ? bg1 : bg2;
+  const backgroundImage = activeView === "single" ? bg1 : bg2;
 
   return (
     <div>
@@ -160,54 +156,11 @@ export const FirstPage = () => {
         </div>
       </div>
 
-      <div className="co-card">
-        <Card className="card1">
-          <Card.Img
-            variant="top"
-            src={viewData[currentIndexes[0]].img}
-            alt={viewData[currentIndexes[0]].title}
-          />
-        </Card>
-
-        
-        <Card className="card2">
-          
-          <Card.Img
-            variant="top"
-            src={viewData[currentIndexes[1]].img}
-            alt={viewData[currentIndexes[1]].title}
-          />
-          
-          <Card.Body>
-            <div>
-                <Card.Title className="card-views"><IoTelescope /> {viewData[currentIndexes[1]].views}</Card.Title>
-                <Card.Title className="card-visits"><IoFootstepsSharp /> {viewData[currentIndexes[1]].visits}</Card.Title>
-                <Card.Title className="card-links"><GoLocation /> {viewData[currentIndexes[1]].link}</Card.Title>
-            </div>
-            
-            <Card.Title className="card-location"><GoLocation /> {viewData[currentIndexes[1]].location}</Card.Title>
-            <Card.Title>{viewData[currentIndexes[1]].title}</Card.Title>
-            <div className="card-icons1">
-                <FaLink />
-            </div>
-            <div className="card-icons2">
-                <FaRegBookmark />
-            </div>
-            <div className="card-icons3">
-                <ImArrowUpRight />
-            </div>
-
-          </Card.Body>
-        </Card>
-
-        <Card className="card3">
-          <Card.Img
-            variant="top"
-            src={viewData[currentIndexes[2]].img}
-            alt={viewData[currentIndexes[2]].title}
-          />
-        </Card>
-      </div>
+      <ImageSlider
+        viewData={viewData}
+        currentIndexes={currentIndexes}
+        direction={direction}
+      />
 
       <div className="navigation-buttons">
         <button className="prev-button" onClick={movePrevious}>
@@ -218,35 +171,10 @@ export const FirstPage = () => {
         </button>
       </div>
 
-        {/* <div className="carousel">
-        <Carousel />
-        </div> */}
-
       <div className="fp-pyt">
-            <img src={svgplan} alt="direction" />
-            <p>Plan Your Trip</p>
+        <img src={svgplan} alt="direction" />
+        <p>Plan Your Trip</p>
       </div>
-
-
-      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-        <div className="sidebarcontent">
-          <button className="closebtn" onClick={closeSidebar}>
-            <MdClose />
-          </button>
-          <div className="sidebar-contents">
-            <div>Beaches</div>
-            <div>Deserts</div>
-            <div>Mountains</div>
-            <div>Iconic cities</div>
-          </div>
-        </div>
-      </div>
-
-      <div className={`sidebararrow ${isOpen ? 'hide' : ''}`} onClick={toggleSidebar}>
-        <MdKeyboardDoubleArrowRight className="sidearrowicon" />
-      </div>
-      
-      
     </div>
   );
 };
--- trip/src/index.js
+++ trip/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Details } from './Components/Details';
 import { LandingPage } from './Components/LandingPage';
+import { FirstPage } from './Components/FirstPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
